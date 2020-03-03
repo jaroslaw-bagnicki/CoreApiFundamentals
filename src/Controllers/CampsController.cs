241542857c5c0c18069b47aed7c5ae6bd2c954b4
Jarosław Bagnicki <jbagnicki@future-processing.com>
2020-03-03 12:06:55 +0100

    Add mapping Camp entity to Camp model

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
+using AutoMapper;
 using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,10 +12,12 @@ namespace CoreCodeCamp.Controllers
     public class CampsController : ControllerBase
     {
         private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
 
-        public CampsController(ICampRepository repository)
+        public CampsController(ICampRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         [HttpGet]
@@ -22,7 +26,10 @@ namespace CoreCodeCamp.Controllers
             try
             {
                 var results = await _repository.GetAllCampsAsync();
-                return Ok(results);
+
+                var models = _mapper.Map<CampModel[]>(results);
+
+                return Ok(models);
             }
             catch (Exception)
             {
