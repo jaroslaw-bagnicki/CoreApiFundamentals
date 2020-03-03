f084a1d36e6cec5b3c65aab3afc83df5ce2e62b5
Jarosław Bagnicki <jbagnicki@future-processing.com>
2020-03-03 11:34:41 +0100

    Add first endpoint

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _repository;
+
+        public CampsController(ICampRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsAsync();
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+        }
+    }
+}
